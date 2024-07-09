7099f6215322b1c8ae8e53bc1fcc4edc586e9d0b
Leonardo Capristo <leonardo.capristo@hotmail.com>
2024-07-09 20:39:53 -0300

    listas

--- listas.py
+++ listas.py
@@ -11,3 +11,9 @@ minhaLista2 = [1 , 2.2 , "texto", True]
 print(minhaLista2)
 
 print(type(minhaLista2[2]))
+
+# .append()     adiciona um novo elemento na lista
+
+minhaLista3 = [1,2,3]
+minhaLista3.append("adicionando texto")
+print(minhaLista3)
